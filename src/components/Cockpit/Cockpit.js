f6d8644ddc8aae9113849c6cb86b39276cd04b97
Shaowei <zsw22@vip.qq.com>
2017-11-28 20:41:30 +1100

    add higher order components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,29 +1,30 @@
 import React from 'react';
-
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
-const cockpit = ( props ) => {
+const cockpit = (props) => {
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Botton;
     if (props.showPersons) {
-        btnClass = classes.Red;
+        btnClass = [classes.Botton, classes.Red].join(" ");
     }
 
-    if ( props.persons.length <= 2 ) {
-      assignedClasses.push( classes.red ); // classes = ['red']
+    if (props.persons.length <= 2) {
+        assignedClasses.push(classes.red); // classes = ['red']
     }
-    if ( props.persons.length <= 1 ) {
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+    if (props.persons.length <= 1) {
+        assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
-        <div className={classes.Cockpit}>
-            <h1>{ props.appTitle }</h1>
-            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+        <Aux>
+            <h1>{props.appTitle}</h1>
+            <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
                 className={btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
-        </div>
+                onClick={props.clicked}>Toggle Persons
+            </button>
+        </Aux>
     );
 };
 
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,23 +1,23 @@
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 
 import Person from './Person/Person';
 
 class Persons extends PureComponent {
-    constructor( props ) {
-        super( props );
-        console.log( '[Persons.js] Inside Constructor', props );
+    constructor(props) {
+        super(props);
+        console.log('[Persons.js] Inside Constructor', props);
     }
 
-    componentWillMount () {
-        console.log( '[Persons.js] Inside componentWillMount()' );
+    componentWillMount() {
+        console.log('[Persons.js] Inside componentWillMount()');
     }
 
-    componentDidMount () {
-        console.log( '[Persons.js] Inside componentDidMount()' );
+    componentDidMount() {
+        console.log('[Persons.js] Inside componentDidMount()');
     }
 
-    componentWillReceiveProps ( nextProps ) {
-        console.log( '[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps );
+    componentWillReceiveProps(nextProps) {
+        console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
     }
 
     // shouldComponentUpdate ( nextProps, nextState ) {
@@ -28,24 +28,24 @@ class Persons extends PureComponent {
     //     // return true;
     // }
 
-    componentWillUpdate ( nextProps, nextState ) {
-        console.log( '[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState );
+    componentWillUpdate(nextProps, nextState) {
+        console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
     }
 
-    componentDidUpdate () {
-        console.log( '[UPDATE Persons.js] Inside componentDidUpdate' );
+    componentDidUpdate() {
+        console.log('[UPDATE Persons.js] Inside componentDidUpdate');
     }
 
-    render () {
-        console.log( '[Persons.js] Inside render()' );
-        return this.props.persons.map( ( person, index ) => {
+    render() {
+        console.log('[Persons.js] Inside render()');
+        return this.props.persons.map((person, index) => {
             return <Person
-                click={() => this.props.clicked( index )}
+                click={() => this.props.clicked(index)}
                 name={person.name}
                 age={person.age}
                 key={person.id}
-                changed={( event ) => this.props.changed( event, person.id )} />
-        } );
+                changed={(event) => this.props.changed(event, person.id)}/>
+        });
     }
 }
 
